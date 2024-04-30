c58d442fb47bcdf4058bce5b4bf16cf119e67b3d
marcellinow <marcellino.gabriel12@gmail.com>
2024-04-30 16:10:19 +0700

    perbaikan posisi file

--- src/AreaType.java
+++ src/AreaType.java
@@ -1,6 +1,6 @@
 public enum AreaType {
     PROTECTED_AREA, // Area yang harus dilindungi pemain
     PLANTABLE_AREA, // Area yang dapat ditanami
-    WATER_AREA,     // Area yang harus ditanami dengan Lilypad terlebih dahulu
-    ZOMBIE_SPAWN    // Area tempat muncul zombie
+    WATER_AREA, // Area yang harus ditanami dengan Lilypad terlebih dahulu
+    ZOMBIE_SPAWN // Area tempat muncul zombie
 }
