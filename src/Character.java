708caffe1f461ca4c9e670ab52488df8ec8eea4d
DAPP <130123609+muhammaddaffa175@users.noreply.github.com>
2024-05-01 09:56:22 +0700

    Update Character.java

--- src/Character.java
+++ src/Character.java
@@ -1,3 +1,4 @@
+
 public class Character {
       public String name;
       public int health;
@@ -8,21 +9,29 @@ public class Character {
 public Character(String name, int health, int attack_damage, int attack_speed)
 {
       this.name = name;
-      this.health = health;
-      this.attack_damage = attack_damage;
-      this.attack_speed = attack_speed;
+      this.health = Math.max (0, health); //memastikan health is not negative
+      this.attack_damage = Math.max(0, attack_damage); //memastikan attack_speed is not negative
+      this.attack_speed = Math.max(0, attack_speed); //memastikan attack_speed is not negative
+
 }
 
       public void setName(String nama){this.name=nama;}
       public String getName(){return this.name;}
 
-      public boolean isDead() {if (this.getHealth()<=0) return true; else return false;}
-
-      public void reduceHealth(int damage) {this.health-=damage;}
+      
+      public void setHealth(int health) {this.health = Math.max(0, health);}
       public int getHealth() {return this.health;}
 
-      
+
+      public void setAttackDamage(int attack_damage) {this.attack_damage = Math.max(0, attack_damage);}
       public int getAttackDamage()  {return this.attack_damage;}
+
+
+      public void setAttackSpeed(int attack_speed) {this.attack_speed = Math.max(0,attack_speed);}
       public int getAttackSpeed() {return this.attack_speed;}
+
+
+      public void reduceHealth(int damage) {this.health-=damage;}
+      public boolean isDead() {if (this.getHealth()<=0) return true; else return false;}
 
 }
