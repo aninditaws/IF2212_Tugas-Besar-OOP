6a6322e788c26040421522f198b2463d2fa8f900
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-05-13 20:05:27 +0700

    pp

--- src/Plant/SnowPea.java
+++ src/Plant/SnowPea.java
@@ -1,6 +1,8 @@
 package Plant;
 
 public class SnowPea extends Plant{
+      
+      
       public SnowPea(String name, int health, int attack_damage, int attack_speed, int cost, int range, int cooldown) {
             super(name, health, attack_damage, attack_speed, cost, range, cooldown);
         }
