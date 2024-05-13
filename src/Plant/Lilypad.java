be79797928414e1b1e8742d3589fd93833c3689a
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-05-13 20:26:48 +0700

    update lilypad

--- src/Plant/Lilypad.java
+++ src/Plant/Lilypad.java
@@ -10,4 +10,22 @@ public class Lilypad extends Plant {
       public boolean getPlanted(){
             return this.planted;
       }  
+
+      public void is_it_planted(Plant plant) {
+            if (!planted && plant instanceof Lilypad) { // Jika lilypad belum terpasang tanaman dan yang akan ditanam adalah lilypad
+                System.out.println(plant.getName() + " ditanam di " + this.getName() + "pada baris dan kolom"); // Menanam tanaman di atas lilypad
+                planted = true; // Menandakan bahwa lilypad sudah terpasang tanaman
+            } else {
+                System.out.println("Cannot plant " + plant.getName() + " on " + this.getName() + ", only Lilypad allowed."); // Jika lilypad sudah terpasang tanaman atau yang akan ditanam bukan lilypad
+            }
+        }
+
+      public void removePlant() {
+            if (planted) { // Jika lilypad sudah terpasang tanaman
+                System.out.println("Plant removed from " + this.getName()); // Mencopot tanaman dari lilypad
+                planted = false; // Menandakan bahwa lilypad sudah tidak terpasang tanaman lagi
+            } else {
+                System.out.println("No plant to remove from " + this.getName()); // Jika lilypad belum terpasang tanaman
+            }
+        }
 }
