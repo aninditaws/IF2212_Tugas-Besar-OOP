f334489ec644b79fa359a43a96828d8f1694bf96
aninditaws <18222128@std.stei.itb.ac.id>
2024-05-14 12:29:12 +0700

    add: menambah sun dalam interval

--- src/Sun/Sun.java
+++ src/Sun/Sun.java
@@ -1,11 +1,18 @@
 package Sun;
 
-public final class Sun {
+import Subscriber.EventChannel;
+import Subscriber.Subscriber;
+
+public final class Sun implements Subscriber {
     private int totalSun;
     private static Sun instance;
 
+    private int randomGeneratedInterval = 0;
+    private int doUpdate = 0;
+
     private Sun() {
         totalSun = 0;
+        EventChannel.getInstance().subscribe(this);
     }
 
     public static Sun getInstance() {
@@ -28,6 +35,23 @@ public final class Sun {
             totalSun -= cost;
         } else {
             throw new Exception("Not enough sun to spend");
+        }
+    }
+
+    public void generateRandomInterval() {
+        int max = 10;
+        int min = 5;
+        randomGeneratedInterval = (int) ((Math.random() * (max - min)) + min);
+    }
+
+    @Override
+    public void update() {
+        // Memberikan sun dalam jangka waktu yang random
+        doUpdate += 1;
+        if (doUpdate == randomGeneratedInterval) {
+            doUpdate = 0;
+            generateRandomInterval();
+            generateSun();
         }
     }
 }
