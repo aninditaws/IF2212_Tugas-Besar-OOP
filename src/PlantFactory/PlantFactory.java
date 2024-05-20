dc1dbc556867f6a96b5d2020a4ad4125c9f2f33f
aninditaws <santosoanindita1@gmail.com>
2024-05-20 23:40:33 +0700

    add: plant factory FIXED

--- src/PlantFactory/PlantFactory.java
+++ src/PlantFactory/PlantFactory.java
@@ -9,6 +9,24 @@ public class PlantFactory {
         switch (plantType) {
             case SUNFLOWERTYPE:
                 return new Sunflower(position);
+            case CHERRYBOMBTYPE:
+                return new CherryBomb(position);    
+            case JALAPENOTYPE:
+                return new Jalapeno(position);
+            case LILYPADTYPE:
+                return new Lilypad(position);    
+            case PEASHOOTERTYPE:
+                return new Peashooter(position);
+            case REPEATERTYPE:
+                return new Repeater(position);
+            case SNOWPEATYPE:
+                return new SnowPea(position);
+            case SQUASHTYPE:
+                return new Squash(position);
+            case TALLNUTTYPE:
+                return new Tallnut(position);
+            case WALLNUTTYPE:
+                return new Wallnut(position);
             default:
                 return new Sunflower(position);
         }
