df6b5baad0cec2160cf38424970cd3c8fccc08a7
aninditaws <18222128@std.stei.itb.ac.id>
2024-05-20 19:43:58 +0700

    fix: nama cherrybomb

--- src/Plant/CherryBomb.java
+++ src/Plant/CherryBomb.java
@@ -3,7 +3,7 @@ package Plant;
 public class CherryBomb extends Plant{
 
       public CherryBomb() {
-            super("Cherrrybomb", 100, 5000, 0, 150, 12, 25, "ImagePvZ/Plant/PlantzImg/CherryBomb.png");
+            super("Cherrybomb", 100, 5000, 0, 150, 12, 25, "ImagePvZ/Plant/PlantzImg/CherryBomb.png");
         }
       
 }
