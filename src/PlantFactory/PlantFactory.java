94120beb195f913c34110758a359a2de3260cc8f
aninditaws <18222128@std.stei.itb.ac.id>
2024-05-20 23:55:12 +0700

    add: PlantFactory getPlantType

--- src/PlantFactory/PlantFactory.java
+++ src/PlantFactory/PlantFactory.java
@@ -1,7 +1,9 @@
 package PlantFactory;
 import Plant.*;
+import Sun.Sun;
 
 import java.awt.*;
+import java.util.Objects;
 
 public class PlantFactory {
 
@@ -29,6 +31,14 @@ public class PlantFactory {
                 return new Wallnut(position);
             default:
                 return new Sunflower(position);
+        }
+    }
+
+    public PlantType getPlantType(Plant plant) {
+        if (plant instanceof Sunflower) {
+            return PlantType.SUNFLOWERTYPE;
+        } else {
+            return PlantType.SUNFLOWERTYPE;
         }
     }
 }
