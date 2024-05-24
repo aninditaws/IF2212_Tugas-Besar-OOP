d3145f316929a247e52cbd9fb83e5061f81d77b7
DAPP <130123609+muhammaddaffa175@users.noreply.github.com>
2024-05-24 18:42:55 +0700

    Update DolphinRiderZombie.java

--- src/Zombie/DolphinRiderZombie.java
+++ src/Zombie/DolphinRiderZombie.java
@@ -3,9 +3,15 @@ package Zombie;
 import java.awt.*;
 
 public class DolphinRiderZombie extends Zombie {
+    private boolean canJump;
+
     public DolphinRiderZombie(Point position) {
         super("Dolphin Rider Zombie", 175, 100, 1, true, position);
     }
 
+
     // Bisa jump/destroy tanaman
+    public void setCanJump(boolean canJump){
+        this.canJump = canJump;
+    }
 }
