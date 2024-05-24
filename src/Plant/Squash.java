dd489a1b60f5c57430dec864281bb876abaf3e9f
aninditaws <18222128@std.stei.itb.ac.id>
2024-05-24 12:44:15 +0700

    fix: squash logic

--- src/Plant/Squash.java
+++ src/Plant/Squash.java
@@ -19,7 +19,7 @@ public class Squash extends Plant {
       @Override
       public void attack(GameMap<Object> gameMap) {
             super.attack(gameMap);
-            List<Object> nextColZombies = gameMap.getEntities(position.y, (Math.max(position.x, 10))).stream().filter(entity -> entity instanceof Zombie).collect(Collectors.toList());
+            List<Object> nextColZombies = gameMap.getEntities(position.y, (Math.min(position.x + 1, 10))).stream().filter(entity -> entity instanceof Zombie).collect(Collectors.toList());
             nextColZombies.forEach(zombie -> {
                   ((Zombie) zombie).reduceHealth(attack_damage);
                   System.out.println(String.format("Squash attacks %s", ((Zombie) zombie).name));
