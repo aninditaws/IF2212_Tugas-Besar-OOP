4cb2c5e9cbbab76cb3c0dca14b42350988563f7a
sFrans21 <118550565+sFrans21@users.noreply.github.com>
2024-05-24 16:55:50 +0700

    Update PolevaultingZombie.java

--- src/Zombie/PolevaultingZombie.java
+++ src/Zombie/PolevaultingZombie.java
@@ -1,12 +1,30 @@
 package Zombie;
 
 import java.awt.*;
+import java.util.stream.Collectors;
+
+import Game.GameMap;
+import Plant.Plant;
+import Plant.Tallnut;
 
 public class PolevaultingZombie extends Zombie {
+
+    private boolean canJump;
 
     public PolevaultingZombie(Point position) {
         super("Pole Vaulting Zombie", 175, 100, 1, false, position);
     }
 
-    // Dapat melakukan lompatan
+    public void jump(){
+        if (canJump==true && Plant.name == "Tallnut" ) {
+            // Dapat melakukan lompatan, zombie nembus tanaman di depannya
+            gameMap.moveEntity(position.getX(),position.getY(), (position.getX() + 1), (position.getY() + 1));
+            System.out.println(String.format("Pole Vault zombie jumped to %d from %d, %d to %d.", position.getX(), (position.getX() + 1), position.getY(), (position.getY() + 1)));
+
+        }
+    }
+
+    public void setCanJump(boolean canJump){
+        this.canJump = canJump;
+    }
 }
